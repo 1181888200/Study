95183121ca7443aba0e01779bfd1d65f018dcaff
lwlong <1181888200@qq.com>
2018-04-12 15:30:11 +0800

    事物管理

--- Spring-boot/spring-boot-dataSource/src/main/java/com/lwl/boot/SpringBootDataSourceApplication.java
+++ Spring-boot/spring-boot-dataSource/src/main/java/com/lwl/boot/SpringBootDataSourceApplication.java
@@ -1,13 +1,29 @@
 package com.lwl.boot;
 
+import javax.sql.DataSource;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("com.lwl.boot.mybatis.mapper")
+//@EnableTransactionManagement
+@ImportResource("classpath:applicationContent-tx.xml")
 public class SpringBootDataSourceApplication {
 
+	//通过xml方式需要提供该方法
+	@Bean
+	public PlatformTransactionManager txManager(DataSource dataSource) {
+		DataSourceTransactionManager txManager = new DataSourceTransactionManager(dataSource);
+		return txManager;
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataSourceApplication.class, args);
 	}
--- Spring-boot/spring-boot-dataSource/src/main/java/com/lwl/boot/mybatis/service/impl/PermissionServiceImpl.java
+++ Spring-boot/spring-boot-dataSource/src/main/java/com/lwl/boot/mybatis/service/impl/PermissionServiceImpl.java
@@ -4,17 +4,20 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.lwl.boot.mybatis.mapper.PermissionMapper;
 import com.lwl.boot.mybatis.model.Permission;
 import com.lwl.boot.mybatis.service.IPermissionService;
 
 @Service
+//@Transactional
 public class PermissionServiceImpl implements IPermissionService {
 
 	@Autowired
 	private PermissionMapper permissionMapper;
 	
+//	@Transactional(readOnly=true)
 	@Override
 	public List<Permission> selectAll() {
 		return permissionMapper.selectAll();
@@ -22,7 +25,8 @@ public class PermissionServiceImpl implements IPermissionService {
 
 	@Override
 	public void insert(Permission permission) {
-		permissionMapper.insert(permission);		
+		permissionMapper.insert(permission);
+		int i = 1 /0 ;
 	}
 
 }
